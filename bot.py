aee7d8400816cd068f15af5c13671b83198b43a3
LapisMYT <nikitagavrilin005@gmail.com>
2023-09-12 13:45:10 +0300

    Init

---
+++ bot.py
@@ -0,0 +1,18 @@
+import telebot
+
+with open('token.txt', 'r') as file:
+    token = file.read().strip()
+
+with open('link.txt', 'r') as file:
+    link = file.read().strip()
+
+bot = telebot.TeleBot(token)
+
+@bot.channel_post_handler()
+def add_link_to_post(message):
+    channel_username = message.chat.username
+    post_id = message.message_id
+    caption = message.caption if message.caption else ''
+    bot.edit_message_caption(chat_id=channel_username, message_id=post_id, caption=caption + '\n\n' + link, parse_mode='markdown')
+
+bot.infinity_polling()
