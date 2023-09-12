bc78a93763b2f75bfeb070603e0f269f60d2c35d
LapisMYT <nikitagavrilin005@gmail.com>
2023-09-12 23:42:31 +0300

    Fixed bug

--- bot.py
+++ bot.py
@@ -8,15 +8,18 @@ with open('link.txt', 'r') as file:
 
 bot = telebot.TeleBot(token)
 
-@bot.channel_post_handler(content_types=['text', 'photo'])
+@bot.channel_post_handler(content_types=['text'])
 def add_link_to_post(message):
     channel_id = message.chat.id
     post_id = message.message_id
-    if hasattr(message, 'caption'):
-        caption = message.caption
-        bot.edit_message_caption(chat_id=channel_id, message_id=post_id, caption=caption + '\n\n' + link, parse_mode='markdown')
-    else:
-        text = message.text
-        bot.edit_message_text(chat_id=channel_id, message_id=post_id, text=text + '\n\n' + link, parse_mode='markdown')
+    text = message.text
+    bot.edit_message_text(chat_id=channel_id, message_id=post_id, text=text + '\n\n' + link, parse_mode='markdown')
+
+@bot.channel_post_handler(content_types=['photo'])
+def add_link_to_caption(message):
+    channel_id = message.chat.id
+    post_id = message.message_id
+    caption = message.caption
+    bot.edit_message_caption(chat_id=channel_id, message_id=post_id, caption=caption + '\n\n' + link, parse_mode='markdown')
 
 bot.infinity_polling()
